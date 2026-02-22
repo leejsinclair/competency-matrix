0f0d20d30842b83fcc1a732fe24ae17047fee75c
Lee Sinclair <lee.j.sinclair@gmail.com>
2026-02-22 12:53:17 +0000

    chore: add ESLint configuration and VSCode recommendations

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'artifacts/**',
+      'logs/**',
+      'models/**',
+      'database/**'
+    ]
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^(_|error)$'
+        }
+      ]
+    }
+  }
+];
